879dbee44889e64ef6607d06ea3a38643091218a
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-14 12:53:16 +0530

    feat:json-server

---
+++ src/script.js
@@ -0,0 +1,66 @@
+function getMountnode() {
+
+
+    const elemetID = "root"
+    let MOUNT_NODE = document.getElementById("root")
+
+    if (!MOUNT_NODE) {
+        const element = document.createElement('div')
+        element.innerHTML = `<div id="${elemetID}"></div>`
+        document.body.appendChild(element)
+        MOUNT_NODE = document.getElementById(elemetID)
+
+    }
+    console.log('root success')
+
+    return MOUNT_NODE
+}
+
+getMountnode()
+
+
+window.campain={
+  app_id:1,
+  domain:window.location.href,
+
+}
+
+function FecthingData(){
+
+    fetch(`http://localhost:3004/popups/${window.campain.app_id}`).then((response)=>{
+        window.campainattribute=response.data
+        }).catch((error)=>{
+            console.log(error)
+        })
+}
+
+FecthingData()
+
+function Checking(){
+
+    switch(window.campainattribute.type)
+    {
+        case "Exitpopups":
+            return Exitintent()
+            
+            default:
+                return null
+    }
+}
+
+Checking()
+
+function  Exitintent(){
+
+    document.onmouseout=(event)=>{
+        if(event.screenY<=200){
+            let main=document.getElementById('root');
+            let  div=document.createElement('div');
+             div.innerHTML=`${window.campainattribute._html}`
+
+             main.appendChild(div)
+             return main
+
+        }
+    }
+}
