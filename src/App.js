a8d2c21ca6dcbabb235b949de2c674a1d0ca5203
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-15 23:41:23 +0530

    feat:added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import Checking from "./component/Checking";
-// import useStore from "./store";
+import useStore from "./store";
 
 function App() {
-  const[campaign,setCampaign]=useState()
-  // const setCampain=useStore((state)=>state.setCampain)
+  // const[campaign,setCampaign]=useState()
+  const campain=useStore((state)=>state.campain)
+  const setCampain=useStore((state)=>state.setCampain)
   
    
   const fetch=async()=>{
     try {
         let result=await axios.get(`http://localhost:3004/popups/${window.campain.app_id}`);
         console.log(result.data)
-        setCampaign(result.data)
+        setCampain(result.data)
      } catch (error) {
         console.log(error)
      }
@@ -22,7 +23,7 @@ useEffect(()=>{
     fetch()
 },[])
 return (
-<>{Checking(campaign)}</>
+  campain ? <><Checking campaign={campain}/></>:null
 )
 }
 
