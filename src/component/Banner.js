5b0d1cd88baef975210709851e594fbc31654a06
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 13:15:18 +0530

    add

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -2,14 +2,14 @@
 
 const Banner = ({html}) => {
   
-    document.onmouseenter=()=>{
+  
         let main=document.getElementById('root');
                let  div=document.createElement('div');
                div.innerHTML=`${html}`
                main.appendChild(div)
 
                return main
-    }
+    
 }
 
 export default Banner
