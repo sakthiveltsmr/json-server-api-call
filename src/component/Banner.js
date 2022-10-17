253aaf18431145d981c54387181de6c2b978c240
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 22:29:39 +0530

    .

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,27 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Structure from "./structure";
 
 
 const Banner = ({html}) => {
-  const[show,setShow]=useState(true)
-  const Ban=()=>{
+//   const[show,setShow]=useState(true)
+//   const Ban=()=>{
 
-      if(show){
-        console.log("bans")
-          // let main=document.getElementById('root');
-          // let  div=document.createElement('div');
-          // div.innerHTML=`${html}`
-          // main.appendChild(div)
-          setShow(false)
-          return <>
-           <Structure html={html}/>
-          </>
-        }
-  }
+//       if(show){
+//         console.log("bans")
+//           // let main=document.getElementById('root');
+//           // let  div=document.createElement('div');
+//           // div.innerHTML=`${html}`
+//           // main.appendChild(div)
+//           setShow(false)
+//           return <>
+         
+//           </>
+//         }
+//   }
         
     return(
         <>
-       {Ban()}
+       {/* {Ban()} */}
+       <Structure html={html}/>
         </>
     )
 }
