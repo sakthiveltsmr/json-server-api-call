c616131ae0bab6b0a3ecfe13ccedba2fb77da790
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 13:39:45 +0530

    ...

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -2,23 +2,31 @@ import { useState } from 'react'
 
 const Welcome = ({html}) => {
     const[show,setShow]=useState(true)
-  document.onmouseenter=()=>{
-    if(show){
-        let main=document.getElementById('root');
-        let  div=document.createElement('div');
-        div.innerHTML=`${html}`
-        main.appendChild(div)
-       setShow(false)
-       const close= document.getElementById('wclose')
+    const entrypopups=()=>{
 
-//on click event for close the popups
-           close.addEventListener("click",()=>{
-             return div.style.display='none'
-           })    
-        return main
+        document.onmouseenter=()=>{
+            console.log("mouse enter")
+            if(show){
+                let main=document.getElementById('root');
+                let  div=document.createElement('div');
+                div.innerHTML=`${html}`
+                main.appendChild(div)
+                
+                const close= document.getElementById('wclose')
+                
+                //on click event for close the popups
+                close.addEventListener("click",()=>{
+                    return div.style.display='none'
+                })  
+                setShow(false)  
+                return main
+            }
     }
 
   }
+  return(
+    <div>{entrypopups()}</div>
+  )
 }
 
 export default Welcome
