b48965f4aa557d0f5ff1bc7c97220639363836c7
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 21:55:37 +0530

    ...

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -4,16 +4,21 @@ import Structure from "./structure";
 
 const Banner = ({html}) => {
   const[show,setShow]=useState(true)
-  if(show){
-    // let main=document.getElementById('root');
-    // let  div=document.createElement('div');
-    // div.innerHTML=`${html}`
-    // main.appendChild(div)
-   setShow(false)
-    return <Structure html={html}/>
+  const ban=()=>{
+
+      if(show){
+          // let main=document.getElementById('root');
+          // let  div=document.createElement('div');
+          // div.innerHTML=`${html}`
+          // main.appendChild(div)
+          setShow(false)
+          return <Structure html={html}/>
+        }
   }
         
-    
+    return(
+        <>{ban()}</>
+    )
 }
 
 export default Banner
