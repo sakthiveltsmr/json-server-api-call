ae9b063cf38ccbc5eeba3aab4d09420725cbdcda
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 22:38:57 +0530

    ...

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -3,22 +3,7 @@ import Structure from "./structure";
 
 
 const Banner = ({html,show}) => {
-//   const[show,setShow]=useState(true)
-//   const Ban=()=>{
 
-//       if(show){
-//         console.log("bans")
-//           // let main=document.getElementById('root');
-//           // let  div=document.createElement('div');
-//           // div.innerHTML=`${html}`
-//           // main.appendChild(div)
-//           setShow(false)
-//           return <>
-         
-//           </>
-//         }
-//   }
-        
     return(
         <>
       {show?
--- src/component/Exitpopups.js
+++ src/component/Exitpopups.js
@@ -1,28 +1,29 @@
-import useStore from "../store";
+// import useStore from "../store";
+import Structure from "./structure";
 
 const Exitpopups = ({html}) => {
 
     console.log("in html",html)
-    const campaign=useStore((state)=>state.campain)
-    const setCampain=useStore((state)=>state.setCampain)
+    // const campaign=useStore((state)=>state.campain)
+    // const setCampain=useStore((state)=>state.setCampain)
    
     document.onmouseout=(event)=>{
         if(event.clientY<=0){
             console.log(event.clientY)
-           if(campaign.active){
-               setCampain({...campaign,active:false})
-               let main=document.getElementById('root');
-               let  div=document.createElement('div');
-               div.innerHTML=`${html}`
-               main.appendChild(div)
-           const close= document.getElementById('close')
+          return <Structure html={html}/>
+            //    setCampain({...campaign,active:false})
+//                let main=document.getElementById('root');
+//                let  div=document.createElement('div');
+//                div.innerHTML=`${html}`
+//                main.appendChild(div)
+//            const close= document.getElementById('close')
 
-//on click event for close the popups
-           close.addEventListener("click",()=>{
-             return div.style.display='none'
-           })    
-               return main
-            }
+// //on click event for close the popups
+//            close.addEventListener("click",()=>{
+//              return div.style.display='none'
+//            })    
+//                return main
+//             }
 
         }
     }
