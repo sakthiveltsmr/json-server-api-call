eeba0f5213c4f9761aec2aad9f1d7a33feef15d4
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-17 13:18:33 +0530

    add

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,14 +1,17 @@
+import { useState } from "react";
 
 
 const Banner = ({html}) => {
-  
-  
-        let main=document.getElementById('root');
-               let  div=document.createElement('div');
-               div.innerHTML=`${html}`
-               main.appendChild(div)
-
-               return main
+  const[show,setShow]=useState(true)
+  if(show){
+    let main=document.getElementById('root');
+    let  div=document.createElement('div');
+    div.innerHTML=`${html}`
+    main.appendChild(div)
+   setShow(false)
+    return main
+  }
+        
     
 }
 
