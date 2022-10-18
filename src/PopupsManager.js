254bca1f721ec8a0653d18fded5ba0ed9af4546c
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-18 15:42:55 +0530

    add

--- src/PopupsManager.js
+++ src/PopupsManager.js
@@ -3,25 +3,25 @@ import useStore from "./store"
 export const PopupsManager={
 
   
-    OnExitIntent:(props)=>{
-        console.log("object",props);
-        // const html=useStore((state)=>state.html)
-        // const setHtml=useStore((state)=>state.setHtml)
-        // console.log("type",show)
-        // document.addEventListener("mouseout",(e)=>{
-        //     if(e.clientY<=0){
-        //         if(!show){
-        //        return map(html.events,(item)=>{
-        //         if(item.type===type){
-        //           let value={...item,active:true}
-        //          return setHtml({...html,value})
-        //         }
-        //        })
+    OnExitIntent:({show, type })=>{
+        // console.log("object",props);
+        const html=useStore((state)=>state.html)
+        const setHtml=useStore((state)=>state.setHtml)
+        console.log("type",show)
+        document.addEventListener("mouseout",(e)=>{
+            if(e.clientY<=0){
+                if(!show){
+               return map(html.events,(item)=>{
+                if(item.type===type){
+                  let value={...item,active:true}
+                 return setHtml({...html,value})
+                }
+               })
                 
-        //         }
-        //     }
-        //     console.log("html  from onexitintent",html)
-        // })
+                }
+            }
+            console.log("html  from onexitintent",html)
+        })
         
     },
 
