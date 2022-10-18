5c74cc9570b1ff14175999afd24f417961acd2d9
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-18 18:05:54 +0530

    added

--- src/campain/Action.js
+++ src/campain/Action.js
@@ -1,12 +1,12 @@
 import useStore from "../store"
 import map from "lodash/map"
-import { useEffect } from "react"
+
 
 export const Action=(type)=>{
     console.log("types",type)
     const Rule=useStore((state)=>state.Rules)
     const setRule=useStore((state)=>state.setRules)
-    const Active=()=>{
+    const Active=(type)=>{
         const event= map(Rule.events,(item)=>{
             return item.type === type ? { ...item, active:true } : item
      
@@ -14,19 +14,13 @@ export const Action=(type)=>{
             
           setRule({...Rule,events:event})
 
-         
+         console.log("active",Rule)
     }
 
-    useEffect(()=>{
-
-        Active(type)
-        
-        console.log("Rule",Rule)
-
-    },[type])
+    
     
 
-  return null
+  return Active(type)
    
     
   
