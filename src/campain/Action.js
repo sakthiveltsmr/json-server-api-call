bd8c7529d12976eeb90875bafe1366b7f939c0d9
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-18 18:14:36 +0530

    sxc

--- src/campain/Action.js
+++ src/campain/Action.js
@@ -3,18 +3,22 @@ import map from "lodash/map"
 
 
 export const Action=(type)=>{
-    console.log("types",type)
+    // console.log("types",type)
     const Rule=useStore((state)=>state.Rules)
     const setRule=useStore((state)=>state.setRules)
+
+    console.log("Rule", Rule);
+
     const Active=(type)=>{
         const event= map(Rule.events,(item)=>{
+          console.log("item", item);
             return item.type === type ? { ...item, active:true } : item
      
             })
             
           setRule({...Rule,events:event})
 
-         console.log("active",Rule)
+         //console.log("active",Rule)
     }
 
     
