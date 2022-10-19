62ec03dfa673fa495bee8fc3d2c4f70f8435121d
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-10-19 11:30:03 +0530

    ...

--- src/campain/Action.js
+++ src/campain/Action.js
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 
 export const Action=(type)=>{
-    // console.log("types",type)
+    console.log("types",type)
     // const Rule=useStore((state)=>state.Rules)
     // const setRule=useStore((state)=>state.setRules)
 
